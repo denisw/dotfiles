84e10ea3a886afeb2270bf834945f6be6f27d11b
Denis Washington <denis@denisw.de>
2017-03-26 17:22:24 +0200

    [emacs] Add rjsx-mode

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -22,6 +22,7 @@
 (require 'my-core)
 (require 'my-evil)
 (require 'my-helm)
+(require 'my-javascript)
 (require 'my-magit)
 (require 'my-projectile)
 
@@ -33,7 +34,7 @@
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
    (quote
-    (exec-path-from-shell cider clojure-mode helm-projectile evil-magit magit helm evil-leader projectile evil use-package))))
+    (rjsx-mode exec-path-from-shell cider clojure-mode helm-projectile evil-magit magit helm evil-leader projectile evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
