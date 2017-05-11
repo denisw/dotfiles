8bdeb8d871b8dcc7bb7fb6f29c6c8c72aacc2458
Denis Washington <denis@denisw.de>
2017-05-11 08:15:44 +0200

    [emacs] Fix rjsx-mode configuration

--- emacs/.emacs.d/modules/my-javascript.el
+++ emacs/.emacs.d/modules/my-javascript.el
@@ -3,12 +3,10 @@
 (use-package rjsx-mode
   :ensure t
   :mode "\\.jsx?\\'"
-
-  :init
+  :config
   (progn
     (setq js2-basic-offset 2)
     (setq js2-strict-missing-semi-warning nil)
     (setq js2-strict-trailing-comma-warning nil)))
-
 
 (provide 'my-javascript)
