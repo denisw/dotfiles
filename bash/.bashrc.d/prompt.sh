2ab59bc03fbaaccecd2345eae09ca5be76f9935b
Denis Washington <denis@denisw.de>
2018-06-04 22:43:01 +0200

    [bash] Properly use git prompt witin PS1

--- bash/.bashrc.d/prompt.sh
+++ bash/.bashrc.d/prompt.sh
@@ -7,4 +7,4 @@ BLUE="\[$(tput setaf 6)\]"
 RED="\[$(tput setaf 1)\]"
 RESET="\[$(tput sgr0)\]"
 
-PS1="${BOLD}${BLUE}\W${RESET}$(__git_ps1 " (${RED}${BOLD}%s${RESET})") \$${RESET} "
+PS1="${BOLD}${BLUE}\W${RESET}\$(__git_ps1 ' (${RED}${BOLD}%s${RESET})') \$${RESET} "
