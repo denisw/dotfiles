7292ae459b818acc06a418f5518de31ea2c62ba0
Denis Washington <denis.washington@futurice.com>
2018-10-23 16:29:40 +0200

    [bash] Add Android tools to PATH

---
+++ bash/.bashrc.d/android.sh
@@ -0,0 +1,5 @@
+if [ -d "$HOME/Library/Android/sdk" ]; then
+  export ANDROID_HOME="$HOME/Library/Android/sdk"
+  export PATH="$PATH:$ANDROID_HOME/tools"
+  export PATH="$PATH:$ANDROID_HOME/platform-tools"
+fi
