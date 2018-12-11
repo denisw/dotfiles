714851095bb8bf2df8ec75591cfa47aa45d4ad85
Denis Washington <denis.washington@futurice.com>
2018-12-11 10:31:14 +0100

    [emacs] Call package-refresh-contents before use-package installation

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -6,6 +6,7 @@
 
 ;; Initialize use-package
 (when (not (package-installed-p 'use-package))
+  (package-refresh-contents)
   (package-install 'use-package))
 (require 'use-package)
 (setq use-package-always-ensure t)
