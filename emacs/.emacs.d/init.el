a94bb80b340307f8b731084bc73797fe283e2b1b
Denis Washington <denis.washington@futurice.com>
2018-12-11 10:37:40 +0100

    [emacs] Update package-selected-packages

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -37,7 +37,7 @@
     ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(package-selected-packages
    (quote
-    (flycheck evil-leader magit molokai-theme solarized-theme markdown-mode projectile ivy evil rjsx-mode req-package))))
+    (yaml-mode flycheck evil-leader magit molokai-theme solarized-theme markdown-mode projectile ivy evil rjsx-mode req-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
