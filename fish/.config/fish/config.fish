c85a744cfde78fd3a54ab2910eccb2d60a2adb6f
Denis Washington <denis@denisw.de>
2019-06-28 09:30:23 +0200

    [fish] Add Git aliases

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,4 @@
+# Prompt
 function fish_prompt
   set_color $fish_color_cwd
   printf '%s' (basename (prompt_pwd))
@@ -8,3 +9,11 @@ function fish_prompt
   echo -n ' $ '
   set_color normal
 end
+
+# Git aliases
+alias ga='git add'
+alias gapa='git add --patch'
+alias gc='git commit'
+alias gco='git checkout'
+alias gl='git pull'
+alias gp='git push'
