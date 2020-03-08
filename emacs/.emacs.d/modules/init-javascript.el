057d2514ff48afd0e77b6596791e83ea18b5bc6d
Denis Washington <denis@denisw.de>
2020-03-08 11:57:31 +0100

    [emacs] Add TypeScript support

--- emacs/.emacs.d/modules/init-javascript.el
+++ emacs/.emacs.d/modules/init-javascript.el
@@ -1,19 +1,26 @@
-;;; init-javascript.el --- JavaScript development
+;;; init-javascript.el --- JavaScript / TypeScript development
 
+(setq-default js-indent-level 2)
+
+;; TypeScript
+;; https://github.com/emacs-typescript/typescript.el
+(use-package typescript-mode
+  :ensure t
+  :config
+  (setq-default typescript-indent-level 2))
+
+;; Put node_modules/.bin into exec path (e.g., for ESLint)
 ;; https://github.com/codesuki/add-node-modules-path
 (use-package add-node-modules-path
   :ensure t
-  :hook js-mode)
+  :hook (js-mode typescript-mode))
 
+;; Prettier
 ;; https://github.com/prettier/prettier-emacs
 (use-package prettier-js
   :ensure t
   :commands prettier-js-mode
-  :hook (js-mode . prettier-js-mode))
-
-;; Set JavaScript tab width
-(defun my-js-mode-hook ()
-  (setq js-indent-level 2))
-(add-hook 'js-mode-hook 'my-js-mode-hook)
+  :hook ((js-mode . prettier-js-mode)
+         (typescript-mode . prettier-js-mode)))
 
 (provide 'init-javascript)
