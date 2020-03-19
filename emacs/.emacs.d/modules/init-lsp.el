e91cb8a254aafd1807fcf11c0fc8ffa707977c2e
Denis Washington <denis@denisw.de>
2020-03-19 20:49:25 +0100

    [emacs][lsp] Hide full LSP docs

--- emacs/.emacs.d/modules/init-lsp.el
+++ emacs/.emacs.d/modules/init-lsp.el
@@ -3,6 +3,8 @@
 (use-package lsp-mode
   :ensure t
   :hook ((js-mode . lsp-deferred)
-         (typescript-mode . lsp-deferred)))
+         (typescript-mode . lsp-deferred))
+  :init
+  (setq lsp-signature-render-documentation nil))
 
 (provide 'init-lsp)
