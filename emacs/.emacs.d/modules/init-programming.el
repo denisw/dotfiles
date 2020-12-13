0a5ee2a1deb0085ff2ad15abd13ec48ce80b4293
Denis Washington <denis@denisw.de>
2020-12-13 11:36:34 +0100

    [emacs] Add dumb-jump

--- emacs/.emacs.d/modules/init-programming.el
+++ emacs/.emacs.d/modules/init-programming.el
@@ -1,8 +1,14 @@
-;;; init-programming.el --- prog-mode customizations
+;;; init-programming.el --- General programming configuration
 
 (defun my/prog-mode-hook ()
   (display-line-numbers-mode 1))
 
 (add-hook 'prog-mode-hook 'my/prog-mode-hook)
+
+;; https://github.com/jacktasia/dumb-jump
+(use-package dumb-jump
+  :ensure t
+  :config
+  (add-hook 'xref-backend-functions #'dumb-jump-xref-activate))
 
 (provide 'init-programming)
