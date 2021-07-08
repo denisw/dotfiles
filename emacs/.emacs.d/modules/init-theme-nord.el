955b715f43e1fd442cd22ae0c7b58174dfc020c3
Denis Washington <denis@denisw.de>
2021-07-08 22:40:49 +0200

    [emacs] Add Nord theme

---
+++ emacs/.emacs.d/modules/init-theme-nord.el
@@ -0,0 +1,9 @@
+;;; init-theme-nord.el --- Theme: Nord
+
+;; https://github.com/balajisivaraman/emacs-one-themes
+(use-package nord-theme
+  :straight t
+  :config
+  (load-theme 'nord t))
+
+(provide 'init-theme-nord)
