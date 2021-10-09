abca343087df5ba2a820a66983f6b0b7d566fd8e
Denis Washington <denis@denisw.de>
2021-10-09 14:06:59 +0200

    [emacs] Update package versions

--- emacs/.emacs.d/modules/init-lsp.el
+++ emacs/.emacs.d/modules/init-lsp.el
@@ -2,8 +2,13 @@
 
 (use-package lsp-mode
   :straight t
-  :init (setq lsp-keymap-prefix "C-c l")
-  :commands lsp)
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
+  (setq gc-cons-threshold 100000000)
+  (setq read-process-output-max (* 1024 1024))
+  :commands lsp
+  :hook (rust-mode . lsp))
 
 (use-package lsp-ui
   :straight t
