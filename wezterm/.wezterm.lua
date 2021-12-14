cf0dd6e3d0e8f9fc7dbe1b1cd38828a6d340de05
Denis Washington <denis@denisw.de>
2021-12-14 07:38:07 +0100

    [wezterm] Add key binding for closing a pane

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,5 +1,16 @@
+local wezterm = require "wezterm";
+
 return {
   color_scheme = "Dracula",
+
+  keys = {
+    {
+      key = "w",
+      mods = "CTRL|SHIFT",
+      action=wezterm.action{CloseCurrentPane={confirm=true}}
+    }
+  },
+
   unix_domains = {
     {
       name = "unix",
