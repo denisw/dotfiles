4a6edc3f66d9fcce7acb0eecfd2d7fd0f69358eb
Denis Washington <denis@denisw.de>
2022-04-07 23:04:54 +0200

    [emacs] Replace lsp-mode with eglot

--- emacs/.emacs.d/modules/init-lsp.el
+++ emacs/.emacs.d/modules/init-lsp.el
@@ -1,19 +1,6 @@
 ;;; init-lsp.el --- Language Server Protocol integration
 
-(use-package lsp-mode
-  :straight t
-  :init
-  (setq lsp-keymap-prefix "C-c l")
-  (setq lsp-ui-doc-show-with-cursor nil)
-  (setq lsp-headerline-breadcrumb-enable nil)
-  ;; https://emacs-lsp.github.io/lsp-mode/page/performance/
-  (setq gc-cons-threshold 100000000)
-  (setq read-process-output-max (* 1024 1024))
-  :commands lsp
-  :hook (rust-mode . lsp))
-
-(use-package lsp-ui
-  :straight t
-  :commands lsp-ui-mode)
+(use-package eglot
+  :straight t)
 
 (provide 'init-lsp)
