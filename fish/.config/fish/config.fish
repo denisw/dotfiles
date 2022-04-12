74650d25c7dc344a59bd04cf8d40b1c911c70278
Denis Washington <denis.washington@futurice.com>
2022-04-12 17:53:39 +0200

    [fish] Extend PATH

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -13,3 +13,25 @@ alias glog='git log --oneline'
 alias gp='git push'
 alias gp!='git push --force-with-lease'
 alias gst='git status'
+
+#
+# PATH additions
+#
+# (Note that fish_add_path does not add directories that does not exist,
+# so it is safe to use for directories that only exist on some systems.)
+
+# Homebrew
+fish_add_path --append --path /opt/homebrew/bin
+
+# ASDF
+fish_add_path --prepend --path ~/.asdf/shims
+
+# Cargo (Rust)
+fish_add_path --append --path ~/.cargo/bin
+
+# Android SDK
+fish_add_path --append --path ~/Library/Android/sdk/platform-tools
+
+# ~/bin
+fish_add_path --append --path ~/bin
+
