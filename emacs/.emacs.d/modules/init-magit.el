5d6cb08cb3f85ac26e1d74a7143bad6b56b12c91
Denis Washington <denis@denisw.de>
2022-11-02 22:45:25 +0100

    [emacs] Add git-modes package for gitignore-mode

--- emacs/.emacs.d/modules/init-magit.el
+++ emacs/.emacs.d/modules/init-magit.el
@@ -1,8 +1,12 @@
-;;; init-magit.el --- Magit Git client
+;;; init-magit.el --- Magit Git client and modes
 
-;; https://magit.vc
+;; https://github.com/magit/magit
 (use-package magit
   :straight t
   :commands magit-status)
+
+;; https://github.com/magit/git-modes
+(use-package git-modes
+  :straight t)
 
 (provide 'init-magit)
