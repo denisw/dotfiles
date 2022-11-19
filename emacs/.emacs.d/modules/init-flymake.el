d3b1211c294d64f39b2c0c54c25bf8969b92a03f
Denis Washington <denis@denisw.de>
2022-11-19 14:37:33 +0100

    [emacs] Add flymake-collection

--- emacs/.emacs.d/modules/init-flymake.el
+++ emacs/.emacs.d/modules/init-flymake.el
@@ -1,5 +1,11 @@
 ;;; init-flymake.el --- Configuration for Flymake
 
+(use-package flymake-collection
+  :straight t
+  :hook (after-init . flymake-collection-hook-setup)
+  :config
+  (push '(javascript-mode flymake-collection-eslint) flymake-collection-config))
+
 ;; https://github.com/meqif/flymake-diagnostic-at-point
 (use-package flymake-diagnostic-at-point
   :straight t
--- emacs/.emacs.d/modules/init-lsp-eglot.el
+++ emacs/.emacs.d/modules/init-lsp-eglot.el
@@ -2,10 +2,15 @@
 
 (use-package eglot
   :straight t
+  :after typescript-mode
   :config
   (put 'typescript-tsx-mode 'eglot-language-id "typescriptreact")
   (add-to-list
    'eglot-server-programs
    `(typescript-tsx-mode . ("typescript-language-server" "--stdio"))))
+
+;; TODO:
+;; - Add Flymake to eglot-stay-out-of
+;; - Add both flymake-collection-eslint and eglot-flymake-backend for JS/TS buffers
 
 (provide 'init-lsp-eglot)
