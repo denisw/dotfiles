4ba9ff75c5b54bc58bd4026b819c45a687fcf905
Denis Washington <denis@denisw.de>
2023-01-04 22:29:39 +0100

    [neovim] Add vsnip for nvim-cmp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -100,11 +100,24 @@ require('packer').startup(function(use)
   }
 
   use {
+    'hrsh7th/cmp-vsnip',
+    requires = {
+      'hrsh7th/nvim-cmp',
+      'hrsh7th/vim-vsnip'
+    }
+  }
+
+  use {
     'hrsh7th/nvim-cmp',
     config = function()
       local cmp = require('cmp')
 
       cmp.setup {
+        snippet = {
+          expand = function(args)
+            vim.fn['vsnip#anonymous'](args.body)
+          end,
+        },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
           ['<C-f>'] = cmp.mapping.scroll_docs(4),
@@ -113,7 +126,8 @@ require('packer').startup(function(use)
           ['<CR>'] = cmp.mapping.confirm({ select = true }),
         }),
         sources = cmp.config.sources({
-          { name = 'nvim_lsp' }
+          { name = 'nvim_lsp' },
+          { name = 'vsnip' },
         })
       }
     end
