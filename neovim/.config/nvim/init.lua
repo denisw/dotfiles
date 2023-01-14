5bddeb95a4abd47c958d01b5098bec2ca5972680
Denis Washington <denis@denisw.de>
2023-01-14 22:38:41 +0100

    [neovim] Integrate nvim-autopairs into nvim-cmp

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -200,8 +200,12 @@ require('packer').startup(function(use)
 
   use {
     'windwp/nvim-autopairs',
+    after = 'nvim-cmp',
     config = function()
       require('nvim-autopairs').setup {}
+      local cmp = require('cmp')
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
     end
   }
 
