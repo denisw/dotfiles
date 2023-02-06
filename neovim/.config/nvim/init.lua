0b637b0607ac6a7beb3471f5fd549baaa7fb2c98
Denis Washington <denis@denisw.de>
2023-02-06 08:05:41 +0100

    [neovim] Run ESLint / Prettier only if available locally

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -341,13 +341,21 @@ require('packer').startup(function(use)
 
       null_ls.setup {
         sources = {
-          null_ls.builtins.code_actions.eslint,
-          null_ls.builtins.diagnostics.eslint,
-          null_ls.builtins.diagnostics.shellcheck,
+          null_ls.builtins.code_actions.eslint.with({
+            only_local = 'node_modules/.bin',
+          }),
+          null_ls.builtins.diagnostics.eslint.with({
+            only_local = 'node_modules/.bin',
+          }),
+          null_ls.builtins.diagnostics.shellcheck.with({
+            condition = executable_condition('shellcheck'),
+          }),
           null_ls.builtins.diagnostics.ruff.with({
             condition = executable_condition('ruff'),
           }),
-          null_ls.builtins.formatting.prettier,
+          null_ls.builtins.formatting.prettier.with({
+            only_local = "node_modules/.bin",
+          }),
           null_ls.builtins.formatting.yapf.with({
             condition = executable_condition('yapf'),
           }),
