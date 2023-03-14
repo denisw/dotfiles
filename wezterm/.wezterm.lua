29c1335b3137dd71205fe6e6e0e5552d110a71c1
Denis Washington <denis@denisw.de>
2023-03-14 21:29:57 +0100

    [wezterm] Add simple configuration

---
+++ wezterm/.wezterm.lua
@@ -0,0 +1,11 @@
+return {
+  color_scheme = "Dracula (Official)",
+  use_fancy_tab_bar = false,
+  hide_tab_bar_if_only_one_tab = true,
+  window_padding = {
+    left = 2,
+    right = 2,
+    top = 2,
+    bottom = 2,
+  },
+}
