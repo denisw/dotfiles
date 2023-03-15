451aee9a31a7e13e178b6a7b7554887fe9178e2b
Denis Washington <denis@denisw.de>
2023-03-15 18:53:34 +0100

    [neovim] Add trouble.nvim

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -491,6 +491,27 @@ require('packer').startup(function(use)
   }
 
   -- UI
+  --
+
+  use {
+    'folke/trouble.nvim',
+    requires = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require('trouble').setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+
+      options = {silent = true, noremap = true}
+      vim.keymap.set('n', '<leader>xx', '<cmd>TroubleToggle<cr>', options)
+      vim.keymap.set('n', '<leader>xw', '<cmd>TroubleToggle workspace_diagnostics<cr>', options)
+      vim.keymap.set('n', '<leader>xd', '<cmd>TroubleToggle document_diagnostics<cr>', options)
+      vim.keymap.set('n', '<leader>xl', '<cmd>TroubleToggle loclist<cr>', options)
+      vim.keymap.set('n', '<leader>xq', '<cmd>TroubleToggle quickfix<cr>', options)
+      vim.keymap.set('n', 'gR', '<cmd>TroubleToggle lsp_references<cr>', options)
+    end
+  }
 
   use {
     'nvim-lualine/lualine.nvim',
