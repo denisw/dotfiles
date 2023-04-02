92419251006e78b11be136021f9249b9029eda4c
Denis Washington <denis@denisw.de>
2023-04-02 15:52:46 +0200

    [emacs] Add eldoc-box package

--- emacs/.emacs.d/modules/init-eldoc.el
+++ emacs/.emacs.d/modules/init-eldoc.el
@@ -7,4 +7,11 @@
   ;; Prevents long eldoc strings from covering half of the window.
   (setq max-mini-window-height 1))
 
+(use-package eldoc-box
+  :straight t
+  :after evil-collection
+  :config
+  (add-to-list 'evil-collection-key-blacklist "K")
+  (define-key evil-normal-state-map (kbd "K") 'eldoc-box-help-at-point))
+
 (provide 'init-eldoc)
