aa2a679031ed3d4609f9f40574752ebc4dd011b3
Denis Washington <denis@denisw.de>
2023-04-03 07:55:40 +0200

    [neovim] Use Telescope's `find_files` for <C-p>

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -272,7 +272,7 @@ require('packer').startup(function(use)
         }
       }
       local builtin = require('telescope.builtin')
-      vim.keymap.set('n', '<C-p>', builtin.git_files, {})
+      vim.keymap.set('n', '<C-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
