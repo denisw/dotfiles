db62cc2395618836239924758460a4a439b0024d
Denis Washington <denis@denisw.de>
2023-04-12 07:37:38 +0200

    [emacs] Tweak Treemacs root directory face

--- emacs/.emacs.d/modules/init-treemacs.el
+++ emacs/.emacs.d/modules/init-treemacs.el
@@ -6,6 +6,8 @@
   (treemacs-project-follow-mode 1)
   (treemacs-follow-mode 1)
   (global-set-key (kbd "M-0") 'treemacs-select-window)
-  (global-set-key (kbd "C-x t t") 'treemacs))
+  (global-set-key (kbd "C-x t t") 'treemacs)
+  ;; Don't display the root directory larger than the others.
+  (set-face-attribute 'treemacs-root-face nil :height 'unspecified))
 
 (provide 'init-treemacs)
