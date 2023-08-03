ef7fd4befd342b62919408705af9e99d131a7e4a
Denis Washington <denis@denisw.de>
2023-08-03 23:52:51 +0200

    [neovim] Add next/previous keys for Trouble

--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -11,6 +11,8 @@ return {
       vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", options)
       vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", options)
       vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", options)
+      vim.keymap.set("n", "<leader>xj", function() require("trouble").next({skip_groups = true, jump = true}) end, options)
+      vim.keymap.set("n", "<leader>xk", function() require("trouble").previous({skip_groups = true, jump = true}) end, options)
       vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", options)
     end
   },
