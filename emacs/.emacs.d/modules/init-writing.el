9c395f7ed0e4868b0231f57dcac76ffdb97bceea
Denis Washington <denis@denisw.de>
2023-08-22 22:45:35 +0200

    [emacs] Disable visual-fill-column while Olivetti is active
    The two modes are incompatible:
    https://github.com/rnkn/olivetti/issues/27

--- emacs/.emacs.d/modules/init-writing.el
+++ emacs/.emacs.d/modules/init-writing.el
@@ -4,16 +4,25 @@
 (use-package markdown-mode
   :straight t
   :config
-  (setq markdown-enable-wiki-links t))
+  (setq markdown-enable-wiki-links t)
+  (add-hook 'markdown-mode-hook #'visual-line-mode))
 
-;; https://github.com/joostkremers/visual-fill-column
+;; https://codeberg.org/joostkremers/visual-fill-column
 (use-package visual-fill-column
   :straight t
   :config
-  (add-hook 'visual-fill-column-mode-hook #'visual-line-mode))
+  (add-hook 'markdown-mode-hook #'visual-fill-column-mode))
 
 ;; https://github.com/rnkn/olivetti
 (use-package olivetti
-  :straight t)
+  :straight t
+  :after visual-fill-column
+  :config
+  ;; Disable visual-fill-column-mode while Olivetti is enabled.
+  ;; The two modes are incompatible.
+  ;; https://github.com/rnkn/olivetti/issues/27
+  (defun my/before-olivetti-mode (&rest args)
+    (visual-fill-column-mode (if (bound-and-true-p olivetti-mode) +1 -1)))
+  (advice-add 'olivetti-mode :before #'my/before-olivetti-mode))
 
 (provide 'init-writing)
