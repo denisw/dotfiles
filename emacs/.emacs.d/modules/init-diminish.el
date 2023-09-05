6068665acb670785435c329175faea25853a8515
Denis Washington <denis@denisw.de>
2023-09-05 22:04:45 +0200

    [emacs] Add mood-line

--- emacs/.emacs.d/modules/init-diminish.el
+++
@@ -1,6 +0,0 @@
-;;; init-diminish.el --- Clean up modeline with diminish.el
-
-(use-package diminish
-  :straight t)
-
-(provide 'init-diminish)
---
+++ emacs/.emacs.d/modules/init-modeline.el
@@ -0,0 +1,8 @@
+;;; init-modeline.el --- A cleaner modeline
+
+(use-package mood-line
+  :straight t
+  :config
+  (mood-line-mode))
+
+(provide 'init-modeline)
