69d36b0555af23a680b105cdb6d230a851685035
Denis Washington <denis@denisw.de>
2023-09-06 08:17:56 +0200

    [emacs] Disable command echo in shell-mode

--- emacs/.emacs.d/modules/init-shell.el
+++ emacs/.emacs.d/modules/init-shell.el
@@ -1,5 +1,6 @@
-;;; init-shell.el --- Configutation for shell-mode and shell scripting
+;;; init-shell.el --- Configuration for shell-mode and shell scripting
 
+;; Use two-space indent in shell scripts.
 (setq-default sh-basic-offset 2)
 
 ;; Terminal emulation in shell-mode
@@ -7,6 +8,7 @@
 (use-package coterm
   :straight t
   :config
-  (coterm-mode))
+  (coterm-mode)
+  (setq-default comint-process-echoes t))
 
 (provide 'init-shell)
