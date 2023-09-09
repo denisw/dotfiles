b25584ae32ab9420e86e10d3fe48a0397a9763c4
Denis Washington <denis@denisw.de>
2023-09-09 10:59:16 +0200

    [emacs] Add emacs-eat terminal emulator

--- emacs/.emacs.d/modules/init-shell.el
+++ emacs/.emacs.d/modules/init-shell.el
@@ -11,4 +11,17 @@
   (coterm-mode)
   (setq-default comint-process-echoes t))
 
+(use-package eat
+  :after evil
+  :straight (eat :type git
+                 :host codeberg
+                 :repo "akib/emacs-eat"
+                 :files ("*.el" ("term" "term/*.el") "*.texi"
+                         "*.ti" ("terminfo/e" "terminfo/e/*")
+                         ("terminfo/65" "terminfo/65/*")
+                         ("integration" "integration/*")
+                         (:exclude ".dir-locals.el" "*-tests.el")))
+  :init
+  (evil-set-initial-state 'eat-mode 'emacs))
+
 (provide 'init-shell)
