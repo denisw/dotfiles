c190c80d90c75bb715fe0ca4741e26a42e197ce4
Denis Washington <denis@denisw.de>
2023-09-12 08:20:18 +0200

    [emacs] Remove outdated exec-path-from-shell variable

--- emacs/.emacs.d/modules/init-macos.el
+++ emacs/.emacs.d/modules/init-macos.el
@@ -5,7 +5,6 @@
   :if (string-equal system-type "darwin")
   :straight t
   :config
-  (setq exec-path-from-shell-check-startup-files nil)
   (setq exec-path-from-shell-arguments '("-l"))
   (exec-path-from-shell-initialize))
 
