abad5c49eaf85f84034284a528b7e9bc6cdc0adb
Denis Washington <denis@denisw.de>
2023-10-25 23:51:45 +0200

    [neovim] Tweak completion configuration

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -1,36 +1,14 @@
+-- completion.lua:
+-- Plugins and configuration for auto-completion.
+
 return {
-  {
-    'hrsh7th/cmp-nvim-lsp',
-    dependencies = { 'hrsh7th/nvim-cmp' }
-  },
-
-  {
-    'hrsh7th/cmp-buffer',
-    dependencies = { 'hrsh7th/nvim-cmp' },
-  },
-
-  {
-    'hrsh7th/cmp-path',
-    dependencies = { 'hrsh7th/nvim-cmp' },
-  },
-
-  {
-    'hrsh7th/cmp-cmdline',
-    dependencies = { 'hrsh7th/nvim-cmp' },
-  },
-
-  {
-    'dcampos/cmp-snippy',
-    dependencies = {
-      'hrsh7th/nvim-cmp',
-      'dcampos/nvim-snippy',
-    },
-  },
-
+  -- Use nvim-cmp as completion engine.
+  -- https://github.com/hrsh7th/nvim-cmp
   {
     'hrsh7th/nvim-cmp',
     config = function()
       local cmp = require('cmp')
+
       cmp.setup {
         snippet = {
           expand = function(args)
@@ -49,6 +27,60 @@ return {
           { name = 'snippy' },
         })
       }
+
+      cmp.setup.cmdline('/', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = {
+          { name = 'buffer' }
+        }
+      })
+
+      cmp.setup.cmdline(':', {
+        mapping = cmp.mapping.preset.cmdline(),
+        sources = cmp.config.sources({
+          { name = 'path' }
+        }, {
+          { name = 'cmdline' }
+        })
+      })
     end
+  },
+
+  -- LSP completion source.
+  -- https://github.com/hrsh7th/cmp-nvim-lsp
+  {
+    'hrsh7th/cmp-nvim-lsp',
+    dependencies = { 'hrsh7th/nvim-cmp' }
+  },
+
+  -- Buffer word completion source.
+  -- https://github.com/hrsh7th/cmp-buffer
+  {
+    'hrsh7th/cmp-buffer',
+    dependencies = { 'hrsh7th/nvim-cmp' },
+  },
+
+  -- Path completion source.
+  -- https://github.com/hrsh7th/cmp-path
+  {
+    'hrsh7th/cmp-path',
+    dependencies = { 'hrsh7th/nvim-cmp' },
+  },
+
+  -- Command line completion source.
+  -- https://github.com/hrsh7th/cmp-cmdline
+  {
+    'hrsh7th/cmp-cmdline',
+    dependencies = { 'hrsh7th/nvim-cmp' },
+  },
+
+  -- Snippet completion source powered by Snippy.
+  -- https://github.com/dcampos/cmp-snippy
+  {
+    'dcampos/cmp-snippy',
+    dependencies = {
+      'hrsh7th/nvim-cmp',
+      'dcampos/nvim-snippy',
+    },
   },
 }
--- neovim/.config/nvim/lua/plugins/editing.lua
+++ neovim/.config/nvim/lua/plugins/editing.lua
@@ -36,7 +36,32 @@ return {
   -- https://github.com/windwp/nvim-autopairs
   {
     "windwp/nvim-autopairs",
+    dependencies = { 'hrsh7th/cmp-nvim-lsp' },
     opts = {},
+    config = function()
+      require("nvim-autopairs").setup {}
+
+      local cmp = require('cmp')
+      local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+      local handlers = require('nvim-autopairs.completion.handlers')
+
+      cmp.event:on(
+        'confirm_done',
+        cmp_autopairs.on_confirm_done({
+          filetypes = {
+            ["*"] = {
+              ["("] = {
+                kind = {
+                  cmp.lsp.CompletionItemKind.Function,
+                  cmp.lsp.CompletionItemKind.Method,
+                },
+                handler = handlers["*"]
+              }
+            },
+          },
+        })
+      )
+    end
   },
 
   -- HTML tag auto-closing based on Tree-Sitter.
