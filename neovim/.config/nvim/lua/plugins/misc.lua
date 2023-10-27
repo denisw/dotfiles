81112f33396a766e8fa5059abc603d886b8e9350
Denis Washington <denis.washington@futurice.com>
2023-10-27 10:38:27 +0200

    [neovim] Add mini.bufremove

--- neovim/.config/nvim/lua/plugins/misc.lua
+++ neovim/.config/nvim/lua/plugins/misc.lua
@@ -1,4 +1,17 @@
 return {
   "tpope/vim-dispatch",
   "tpope/vim-eunuch",
+
+  -- Commands for removing a buffer without closing the window.
+  -- https://github.com/echasnovski/mini.nvim/blob/main/readmes/mini-bufremove.md
+  {
+    "echasnovski/mini.bufremove",
+    version = "*",
+    keys = {
+      { "<leader>bd", ":lua MiniBufremove.delete()<cr>" },
+    },
+    config = function()
+      require('mini.bufremove').setup()
+    end
+  },
 }
