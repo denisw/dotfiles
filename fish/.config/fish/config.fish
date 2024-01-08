399b649bc4bb4ed48acda27590844ffc87ae3e81
Denis Washington <denis.washington@futurice.com>
2024-01-08 17:03:57 +0100

    [fish] Extend $PATH for various tools

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,30 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+# Homebrew
+if test -d /opt/homebrew/bin
+    fish_add_path /opt/homebrew/bin
+end
+
+# Cargo (Rust)
+if test -d "$HOME/.cargo/env"
+    fish_add_path "$HOME/.cargo/env"
+end
+
+# n (Node.js)
+if type -q n
+    set -x N_PREFIX "$HOME/.n"
+    fish_add_path "$N_PREFIX/bin"
+end
+
+# ~/bin
+if test -d "$HOME/bin"
+    fish_add_path --append "$HOME/bin"
+end
+
+# ~/.local/bin
+if test -d "$HOME/.local/bin"
+    fish_add_path --append "$HOME/.local/bin"
+end
+
+# direnv
+if type -q direnv
+    direnv hook fish | source
 end
