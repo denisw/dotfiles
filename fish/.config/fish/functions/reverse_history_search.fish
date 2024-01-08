7fb1627cc57bc15a209d90c71429e2de3f3f9bff
Denis Washington <denis@denisw.de>
2024-01-08 09:12:23 +0100

    [fish] Configure new prompt and remove cruft

--- fish/.config/fish/functions/reverse_history_search.fish
+++
@@ -1,7 +0,0 @@
-# https://github.com/fish-shell/fish-shell/issues/602#issuecomment-470805514
-function reverse_history_search
-  history | fzf --no-sort | read -l command
-  if test $command
-    commandline -rb $command
-  end
-end
