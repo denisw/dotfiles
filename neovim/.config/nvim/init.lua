aa2ca70b603993c25f69c527b6fe39bdeec9fa2c
Denis Washington <denis.washington@futurice.com>
2024-01-18 15:04:01 +0100

    [neovim] Remove vim-polyglot
    It hasn't been updated for two years and messes with Neovim's filetype
    detection. Better to add specific plugins as needed and otherwise rely
    on Neovim's native language support.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -79,6 +79,27 @@ vim.keymap.set("n", "<leader>en", vim.diagnostic.goto_next, bufopts)
 vim.keymap.set("n", "<leader>ep", vim.diagnostic.goto_prev, bufopts)
 vim.keymap.set("n", "<leader>el", vim.diagnostic.setqflist, bufopts)
 
+
+-----------------------------------------------------------------------
+-- Filetype Detection
+-----------------------------------------------------------------------
+
+vim.filetype.add({
+  pattern = {
+    [".*"] = {
+      priority = -math.huge,
+      function(path, bufnr)
+        local content = vim.api.nvim_buf_get_lines(bufnr, 0, 1, false)[1] or ''
+
+        -- Detect Python scripts without extension by their shebang.
+        if vim.regex([[^#!.*python]]):match_str(content) ~= nil then
+          return 'python'
+        end
+      end
+    },
+  },
+})
+
 -----------------------------------------------------------------------
 -- Plugins
 -----------------------------------------------------------------------
--- neovim/.config/nvim/lua/plugins/languages.lua
+++ neovim/.config/nvim/lua/plugins/languages.lua
@@ -1,4 +1,9 @@
 return {
   "prisma/vim-prisma",
-  "sheerun/vim-polyglot",
+
+  -- https://github.com/hashivim/vim-terraform
+  "hashivim/vim-terraform",
+
+  -- https://github.com/carlsmedstad/vim-bicep
+  "carlsmedstad/vim-bicep",
 }
