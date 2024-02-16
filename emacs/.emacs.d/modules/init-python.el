d1d3658741452f7e7226d2622109c30ced3be072
Denis Washington <denis.washington@futurice.com>
2024-02-16 11:14:11 +0100

    [emacs] Remove anaconda-mode in favor of lsp-pyright

--- emacs/.emacs.d/modules/init-python.el
+++ emacs/.emacs.d/modules/init-python.el
@@ -1,10 +1,5 @@
 ;;; init-python.el --- Python programming
 
-;; https://github.com/pythonic-emacs/anaconda-mode
-(use-package anaconda-mode
-  :ensure t
-  :hook (python-mode python-ts-mode))
-
 (use-package pyvenv
   :ensure t)
 
