55021589c2167ccd3f7d03ead7c6a4a2185f4b00
Denis Washington <denis@denisw.de>
2024-03-05 22:13:43 +0100

    [neovim] Install Treesitter grammars asynchronously

--- neovim/.config/nvim/lua/plugins/treesitter.lua
+++ neovim/.config/nvim/lua/plugins/treesitter.lua
@@ -2,10 +2,7 @@ return {
   {
     "nvim-treesitter/nvim-treesitter",
     lazy = false,
-    build = function()
-      local ts_update = require("nvim-treesitter.install").update({ with_sync = true })
-      ts_update()
-    end,
+    build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup {
         ensure_installed = {
@@ -21,6 +18,7 @@ return {
           "vue",
           "yaml",
         },
+        sync_install = false,
         highlight = {
           enable = true,
           disable = { "vimdoc" },
