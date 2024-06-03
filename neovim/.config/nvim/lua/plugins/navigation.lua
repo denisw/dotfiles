57c80503911b93c0569836750da193197256e357
Denis Washington <denis@denisw.de>
2024-06-03 21:48:35 +0200

    [neovim] Upgrade plugins and adapt to Trouble v3

--- neovim/.config/nvim/lua/plugins/navigation.lua
+++ neovim/.config/nvim/lua/plugins/navigation.lua
@@ -75,6 +75,45 @@ return {
   },
 
   {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {},
+    cmd = "Trouble",
+     keys = {
+      {
+        "<leader>xx",
+        "<cmd>Trouble diagnostics toggle<cr>",
+        desc = "Diagnostics (Trouble)",
+      },
+      {
+        "<leader>xX",
+        "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+        desc = "Buffer Diagnostics (Trouble)",
+      },
+      {
+        "<leader>cs",
+        "<cmd>Trouble symbols toggle focus=false<cr>",
+        desc = "Symbols (Trouble)",
+      },
+      {
+        "<leader>cl",
+        "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+        desc = "LSP Definitions / references / ... (Trouble)",
+      },
+      {
+        "<leader>xL",
+        "<cmd>Trouble loclist toggle<cr>",
+        desc = "Location List (Trouble)",
+      },
+      {
+        "<leader>xQ",
+        "<cmd>Trouble qflist toggle<cr>",
+        desc = "Quickfix List (Trouble)",
+      },
+    },
+  },
+
+  {
     "nvim-telescope/telescope.nvim",
     dependencies = {
       "folke/trouble.nvim",
@@ -105,6 +144,8 @@ return {
         end
       end
 
+      local open_with_trouble = require("trouble.sources.telescope").open
+
       require("telescope").setup {
         defaults = {
           path_display = filename_first_path_display,
@@ -113,10 +154,10 @@ return {
             i = {
               ["<esc>"] = "close",
               ["<c-g>"] = "close",
-              ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
+              ["<c-t>"] = open_with_trouble,
             },
             n = {
-              ["<c-t>"] = require("trouble.providers.telescope").open_with_trouble,
+              ["<c-t>"] = open_with_trouble,
             },
           },
         },
--- neovim/.config/nvim/lua/plugins/ui.lua
+++ neovim/.config/nvim/lua/plugins/ui.lua
@@ -1,23 +1,5 @@
 return {
   {
-    "folke/trouble.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
-    config = function()
-      require("trouble").setup {}
-      options = {silent = true, noremap = true}
-      vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>", options)
-      vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", options)
-      vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", options)
-      vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", options)
-      vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", options)
-      vim.keymap.set("n", "<leader>xj", function() require("trouble").next({skip_groups = true, jump = true}) end, options)
-      vim.keymap.set("n", "<leader>xk", function() require("trouble").previous({skip_groups = true, jump = true}) end, options)
-      vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", options)
-    end
-  },
-
-  {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     lazy = false,
