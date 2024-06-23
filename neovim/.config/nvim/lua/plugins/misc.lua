7260e41b2cc1f00337e9e6f3f908846753555448
Denis Washington <denis@denisw.de>
2024-06-23 22:56:37 +0200

    [neovim] Add markdown-preview.nvim

--- neovim/.config/nvim/lua/plugins/misc.lua
+++ neovim/.config/nvim/lua/plugins/misc.lua
@@ -14,4 +14,11 @@ return {
       require('mini.bufremove').setup()
     end
   },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+  },
 }
