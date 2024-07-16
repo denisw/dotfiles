9e5e31b54b69a57c9047a26ceb75c5be0431cc7c
Denis Washington <denis@denisw.de>
2024-07-16 22:54:55 +0200

    [neovim] Add Conjure and vim-jack-in

--- neovim/.config/nvim/lua/plugins/languages.lua
+++ neovim/.config/nvim/lua/plugins/languages.lua
@@ -6,4 +6,24 @@ return {
 
   -- https://github.com/carlsmedstad/vim-bicep
   "carlsmedstad/vim-bicep",
+
+  -- Interactive development environment
+  -- https://github.com/Olical/conjure
+  -- https://github.com/clojure-vim/vim-jack-in
+  {
+    "Olical/conjure",
+    init = function()
+      vim.g["conjure#filetypes"] = {
+        "clojure",
+        "fennel",
+        "lisp",
+        "racket",
+        "scheme",
+      }
+    end,
+  },
+
+  -- Vim port of cider-jack-in for Clojure
+  -- https://github.com/clojure-vim/vim-jack-in
+  "clojure-vim/vim-jack-in",
 }
