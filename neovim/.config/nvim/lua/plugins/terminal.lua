16671c1026edb918c2e1ef3c6d31416b46f7de44
Denis Washington <denis@denisw.de>
2024-08-06 08:37:18 +0200

    [neovim] Add keybindings to toggle terminals as splits

--- neovim/.config/nvim/lua/plugins/terminal.lua
+++ neovim/.config/nvim/lua/plugins/terminal.lua
@@ -9,6 +9,9 @@ return {
         direction = "float",
       }
 
+      vim.keymap.set("n", [[<C-\>h]], ":ToggleTerm direction=horizontal<CR>", { silent = true })
+      vim.keymap.set("n", [[<C-\>v]], ":ToggleTerm direction=vertical size=60<CR>", { silent = true })
+
       function _G.set_terminal_keymaps()
         local opts = {buffer = 0}
         vim.keymap.set("t", [[<C-\><C-\>]], ":ToggleTerm<CR>", opts)
