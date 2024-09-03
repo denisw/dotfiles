7d9f4b2183dfc376e746a1a7fa691a72651b68f7
Denis Washington <denis@denisw.de>
2024-09-03 21:29:07 +0200

    [neovim] Add nvim-cmp-lsp-signature-help

--- neovim/.config/nvim/lua/plugins/completion.lua
+++ neovim/.config/nvim/lua/plugins/completion.lua
@@ -9,6 +9,7 @@ return {
     dependencies = {
       'hrsh7th/cmp-buffer',
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-nvim-lsp-signature-help',
       'hrsh7th/cmp-path',
       'hrsh7th/cmp-cmdline',
     },
@@ -18,6 +19,11 @@ return {
       cmp.setup {
         completion = {
           completeopt = 'menu,menuone,noinsert',
+        },
+        snippet = {
+          expand = function(args)
+            vim.snippet.expand(args.body)
+          end,
         },
         mapping = cmp.mapping.preset.insert({
           ['<C-b>'] = cmp.mapping.scroll_docs(-4),
@@ -32,7 +38,9 @@ return {
         }),
         sources = cmp.config.sources({
           { name = 'nvim_lsp' },
+          { name = 'nvim_lsp_signature_help' },
           { name = 'path' },
+          { name = 'snippets' }
         }, {
           { name = 'buffer' },
         }),
