bfd16b73612b04b7e71c18bdf54feda08afc147b
Denis Washington <denis@denisw.de>
2024-09-03 22:34:22 +0200

    [wezterm] Reformat configuration with StyLua

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -1,16 +1,16 @@
-local wezterm = require('wezterm')
+local wezterm = require("wezterm")
 
 return {
-  front_end = 'WebGpu',
-  color_scheme = 'Catppuccin Mocha',
-  font = wezterm.font('Fira Code'),
-  font_size = 13,
-  window_decorations = 'RESIZE',
-  window_padding = {
-    left = '0.25cell',
-    right = '0.25cell',
-    top = '0.25cell',
-    bottom = '0.25cell',
-  },
-  enable_kitty_keyboard = true,
+	front_end = "WebGpu",
+	color_scheme = 'Catppuccin Mocha',
+	font = wezterm.font("Fira Code"),
+	font_size = 13,
+	window_decorations = "RESIZE",
+	window_padding = {
+		left = "0.25cell",
+		right = "0.25cell",
+		top = "0.25cell",
+		bottom = "0.25cell",
+	},
+	enable_kitty_keyboard = true,
 }
