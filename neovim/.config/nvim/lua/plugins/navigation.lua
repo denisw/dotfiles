cd93f597f120e66bdf16382537645895ef7f406a
Denis Washington <denis@denisw.de>
2024-10-17 22:46:46 +0200

    [neovim] Show current directory in oil.nvim winbar

--- neovim/.config/nvim/lua/plugins/navigation.lua
+++ neovim/.config/nvim/lua/plugins/navigation.lua
@@ -9,11 +9,26 @@ return {
     keys = {
       { "-", "<cmd>Oil<cr>" },
     },
-    opts = {
-      view_options = {
-        show_hidden = true,
-      },
-    },
+    config = function()
+      -- Show the current working directory in the winbar
+      -- https://github.com/stevearc/oil.nvim/blob/master/doc/recipes.md#show-cwd-in-the-winbar
+      function _G.get_oil_winbar()
+        local dir = require("oil").get_current_dir()
+        if dir then
+          return vim.fn.fnamemodify(dir, ":~")
+        else
+          return vim.api.nvim_buf_get_name(0)
+        end
+      end
+      require("oil").setup({
+        view_options = {
+          show_hidden = true,
+        },
+        win_options = {
+          winbar = "%!v:lua.get_oil_winbar()",
+        },
+      })
+    end,
   },
 
   -- Commands for quickly switching between related files.
