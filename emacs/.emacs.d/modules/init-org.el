fe695a2a0511b1210c12ef52de42ccf33998acbf
Denis Washington <denis@denisw.de>
2025-04-27 11:19:33 +0200

    [emacs] Add custom org-capture Todo template

--- emacs/.emacs.d/modules/init-org.el
+++ emacs/.emacs.d/modules/init-org.el
@@ -3,4 +3,12 @@
 (setopt org-directory "~/Documents/Org")
 (setopt org-default-notes-file "~/Documents/Org/inbox.org")
 
+(setq org-capture-templates
+      '(("t"
+         "Todo"
+         entry
+         (file+headline org-default-notes-file "Inbox")
+         "* TODO %?"
+         :empty-lines-after 1)))
+
 (provide 'init-org)
