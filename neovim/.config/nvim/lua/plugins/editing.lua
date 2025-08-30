1145c09c3398c221ea6eb63f86458cde420b7bca
Denis Washington <denis@denisw.de>
2025-08-30 22:52:16 +0200

    [neovim] Enable nvim-autopairs.endwise rules

--- neovim/.config/nvim/lua/plugins/editing.lua
+++ neovim/.config/nvim/lua/plugins/editing.lua
@@ -33,8 +33,13 @@ return {
   {
     "windwp/nvim-autopairs",
     event = "InsertEnter",
-    config = true,
-    opts = {},
+    config = function()
+      local npairs = require("nvim-autopairs")
+      npairs.setup()
+      npairs.add_rules(require("nvim-autopairs.rules.endwise-elixir"))
+      npairs.add_rules(require("nvim-autopairs.rules.endwise-lua"))
+      npairs.add_rules(require("nvim-autopairs.rules.endwise-ruby"))
+    end,
   },
 
   {
